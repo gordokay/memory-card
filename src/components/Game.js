2311c905c4f32b3aeb0e6a68beaf59786c4bfe69
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-02-21 20:56:03 -0500

    Update card styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import Board from "./Board";
 
-const Game = () => {
-  const colors = ["#9883E5", "#F56476", "#5AF9CA", "#C4C7FA", "#FAA381", "#FAFF81", "#C792DF", "#F7B2BD"];
-  const [ cards, setCards ] = useState(colors.map(color => ({ color, isClicked: false })));
-  const shuffleCards = (cardsCopy) => {
-    for(let i = 0; i < cardsCopy.length; i++) {
-      const index = Math.floor(Math.random() * (cardsCopy.length - i)) + i;
-      const temp = cardsCopy[i];
-      cardsCopy[i] = cardsCopy[index];
-      cardsCopy[index] = temp;
-    }
-    return cardsCopy;
+const colors = ["#9883E5", "#F56476", "#5AF9CA", "#C4C7FA", "#FAA381", "#FAFF81", "#C792DF", "#F7B2BD"];
+
+const shuffleCards = (cardsCopy) => {
+  for(let i = 0; i < cardsCopy.length; i++) {
+    const index = Math.floor(Math.random() * (cardsCopy.length - i)) + i;
+    const temp = cardsCopy[i];
+    cardsCopy[i] = cardsCopy[index];
+    cardsCopy[index] = temp;
   }
+  return cardsCopy;
+}
+
+const Game = () => {
+  const [ cards, setCards ] = useState(colors.map(color => ({ color, isClicked: false })));
   const handleClick = (e) => {
     const cardsCopy = cards.slice();
     let targetIndex = cardsCopy.findIndex(card => card.color === e.target.id);
@@ -20,7 +22,11 @@ const Game = () => {
     cardsCopy[targetIndex] = {...cardsCopy[targetIndex], isClicked: true};
     setCards(shuffleCards(cardsCopy));
   }
-  return <Board cards={cards} handleClick={handleClick} />
+  return (
+    <section className="board">
+      <Board cards={ cards } handleClick={ handleClick } />
+    </section>
+  )
 }
 
 export default Game;
